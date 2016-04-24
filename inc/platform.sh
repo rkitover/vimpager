8823a5b8f42eb558ae6e1cb4f401f72187d1297a
Lucas Hoffmann <l-m-h@web.de>
2016-04-24 22:27:13 +0200

    Move finding logic to platform file.

--- inc/platform.sh
+++ inc/platform.sh
@@ -17,4 +17,36 @@ case $(uname -s) in
     *) bsd=1 ;;
 esac
 
+# Find a suitble awk executable.
+if [ -n "$AWK" ] && command -v "$AWK" >/dev/null; then
+    :
+elif command -v gawk >/dev/null; then
+    AWK=gawk
+elif command -v nawk >/dev/null; then
+    AWK=nawk
+elif command -v mawk >/dev/null; then
+    AWK=mawk
+elif [ -x /usr/xpg4/bin/awk ]; then
+    AWK=/usr/xpg4/bin/awk
+elif command -v awk >/dev/null; then
+    AWK=awk
+else
+    echo "ERROR: No awk found!" >&2
+    exit 1
+fi
+
+# Find a suitable sed executable.
+if [ -n "$SED" ] && command -v "$SED" >/dev/null; then
+    :
+elif command -v gsed >/dev/null; then
+    SED=gsed
+elif [ -x /usr/xpg4/bin/sed ]; then
+    SED=/usr/xpg4/bin/sed
+elif command -v sed >/dev/null; then
+    SED=sed
+else
+    echo "ERROR: No sed found!" >&2
+    exit 1
+fi
+
 # vim: sw=4 et tw=0:
