842f746e1095ae15f490d08220ab5fb85d733786
Rafael Kitover <rkitover@gmail.com>
2025-04-12 08:53:59 +0000

    Support using busybox ash as POSIX_SHELL
    Support using busybox ash as POSIX_SHELL, which also fixes the problem
    of an ash package not being available on a Debian-like system such as
    Ubuntu 24.10.
    
    Fix #278
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- inc/prologue.sh
+++ inc/prologue.sh
@@ -10,31 +10,35 @@ fi
 export PATH
 
 if [ -z "$POSIX_SHELL" ]; then
-	if command -v ash >/dev/null; then
-		POSIX_SHELL=ash
-	elif command -v dash >/dev/null; then
-		POSIX_SHELL=dash
-	elif command -v ksh >/dev/null; then
-		POSIX_SHELL=ksh
-	elif [ -x /usr/xpg4/bin/sh ]; then
-		POSIX_SHELL=/usr/xpg4/bin/sh
-	elif command -v dtksh >/dev/null; then
-		POSIX_SHELL=dtksh
-	elif command -v ksh93 >/dev/null; then
-		POSIX_SHELL=ksh93
-	elif command -v bash >/dev/null; then
-		POSIX_SHELL=bash
-	elif command -v zsh >/dev/null; then
-		POSIX_SHELL=zsh
-	fi
+    if command -v ash >/dev/null; then
+        POSIX_SHELL=ash
+    elif command -v dash >/dev/null; then
+        POSIX_SHELL=dash
+    elif command -v ksh >/dev/null; then
+        POSIX_SHELL=ksh
+    elif command -v busybox >/dev/null; then
+        POSIX_SHELL="busybox sh"
+    elif command -v bb >/dev/null; then
+        POSIX_SHELL="bb sh"
+    elif [ -x /usr/xpg4/bin/sh ]; then
+        POSIX_SHELL=/usr/xpg4/bin/sh
+    elif command -v dtksh >/dev/null; then
+        POSIX_SHELL=dtksh
+    elif command -v ksh93 >/dev/null; then
+        POSIX_SHELL=ksh93
+    elif command -v bash >/dev/null; then
+        POSIX_SHELL=bash
+    elif command -v zsh >/dev/null; then
+        POSIX_SHELL=zsh
+    fi
 
-	if [ ! -z "$POSIX_SHELL" ]; then
-		export POSIX_SHELL
-		exec "$POSIX_SHELL" "$0" "$@"
-	else
-		POSIX_SHELL=/bin/sh # hope for the best
-		export POSIX_SHELL
-	fi
+    if [ ! -z "$POSIX_SHELL" ]; then
+        export POSIX_SHELL
+        exec "$POSIX_SHELL" "$0" "$@"
+    else
+        POSIX_SHELL=/bin/sh # hope for the best
+        export POSIX_SHELL
+    fi
 fi
 
 # hopefully we're now POSIX.
